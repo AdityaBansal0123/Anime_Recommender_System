3c00ce48b28cd21d3740d5421b7ad049189ef479
Aditya Bansal <bansaladitya048@gmail.com>
2025-12-14 17:50:21 +0530

    commit

---
+++ src/base_model.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.layers import Activation,BatchNormalization,Input,Embedding,Dot,Dense,Flatten
+from tensorflow.keras.callbacks import ModelCheckpoint,LearningRateScheduler,TensorBoard,EarlyStopping
+from utils.common_functions import *
+from src.logger import get_logger
+from src.custom_exception import CustomException
+
+logger = get_logger(__name__)
+
+class BaseModel:
+    def __init__(self,config_path):
+        try:
+            self.config = read_yaml(config_path)
+            logger.info("Configuration file loaded successfully.")
+        except Exception as e:
+            raise CustomException("Error loading Configuration file",e)
+        
+    def RecommenderNet(self,n_users,n_anime):
+        try:
+            embedding_size = self.config['model']['embedding_size']
+            user = Input(name="user",shape=[1])
+            user_embedding = Embedding(name="user_embedding",input_dim=n_users,output_dim=embedding_size)(user)
+            anime = Input(name="anime",shape=[1])
+            anime_embedding = Embedding(name="anime_embedding",input_dim=n_anime,output_dim=embedding_size)(anime)
+            x = Dot(name="dot_product" , normalize=True , axes=2)([user_embedding,anime_embedding])
+            x = Flatten()(x)
+            x = Dense(1,kernel_initializer='he_normal')(x)
+            x = BatchNormalization()(x)
+            x = Activation("sigmoid")(x)
+
+            model = Model(inputs=[user,anime], outputs=x)
+            model.compile(loss=self.config['model']['loss'],metrics=self.config['model']['metrics'],optimizer=self.config['model']['optimizer'])
+            logger.info("RecommenderNet model built and compiled successfully.")
+            return model
+        except Exception as e:
+            logger.error(f'Error in building RecommenderNet model {e}')
+            raise CustomException("Error in building RecommenderNet model",e)
+    
